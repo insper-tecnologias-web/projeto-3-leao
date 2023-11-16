d18ef2d83f2571d46cb0c5506ec037db7cbe8074
Felipe Maia <felipeaffonsormaia@gmail.com>
2023-11-16 09:45:33 -0300

    back-end
    puxa ai

--- BackEnd/cars/serializers.py
+++ BackEnd/cars/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Car
+from .models import Car, User
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'title']
+        fields = ['id', 'name', 'email', 'password', 'cars']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'user', 'price', 'brand', 'model', 'year', 'fuel', 'fipeCode']
+
+
--- BackEnd/cars/views.py
+++ BackEnd/cars/views.py
@@ -2,18 +2,31 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import Http404
-from .serializers import CarSerializer
+from .serializers import CarSerializer, UserSerializer
 
-from .models import Car
+from .models import Car, User
 
 def index(request):
     return HttpResponse("Olá mundo! Este é o app LEÃOOOOO")
 
 @api_view(['GET', 'POST'])
-def api_note(request, car_id):
+def api_car(request, car_id):
     try:
         car = Car.objects.get(id=car_id)
     except Car.DoesNotExist:
         raise Http404()
     serialized_car = CarSerializer(car)
     return Response(serialized_car.data)
+
+@api_view(['GET', 'POST'])
+def api_users(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serialized_users = UserSerializer(users, many=True)
+        return Response(serialized_users.data)
+    elif request.method == 'POST':
+        serialized_user = UserSerializer(data=request.data)
+        if serialized_user.is_valid():
+            serialized_user.save()
+            return Response(serialized_user.data, status=201)
+        return Response(serialized_user.errors, status=400)
