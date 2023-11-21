dcab8f6fad1d325441fea2690c10abb706bc0203
Lucas Lima <lucasfsl@al.insper.edu.br>
2023-11-21 08:55:07 -0300

    aaa

---
+++ FrontEnd/lioncar/src/components/Home/components/Catalogue/components/CarCard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./carcard.css";
+
+
+function CarCard() {
+
+    return (
+        <div className="main-car-card">
+            <p className="Marca">VW - VolksWagen</p>
+            <p className="Modelo">AMAROK High.CD 2.0 16V TDI 4x4 Dies. Aut</p>
+            <p className="AnoModelo">2014</p>
+            <p className="Fipe">R$ 108.966,00</p>
+        </div>
+    )
+}
+export default CarCard;
---
+++ FrontEnd/lioncar/src/components/Home/components/Catalogue/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./catalogue.css";
+import CarCard from "./components/CarCard";
+
+
+function Catalogue() {
+
+    return (
+        <div className="catalogue-div">
+            <p className="catalogue-title">Carros publicados</p>
+            <section className="catalogue-section">
+                <CarCard></CarCard>
+                <CarCard></CarCard>
+                <CarCard></CarCard>
+                <CarCard></CarCard>
+                <CarCard></CarCard>
+            </section>
+        </div>
+
+    )
+}
+export default Catalogue;
